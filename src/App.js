d748cab97dcb089f4b291d7e159cfebcbf554ff4
AliReza <ali.reza.buet@gmail.com>
2023-08-21 16:33:52 +0600

    Setting up the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoApp from 'components/TodoApp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <TodoApp />
     </div>
   );
 }
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,13 @@
+import TodosLogic from 'components/TodosLogic';
+import Header from 'components/Header'
+import React from 'react';
+
 const TodoApp = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
-      <p>I am in a React Component!</p>
-    </div>
+    <React.Fragment>
+      <Header />
+      <TodosLogic />
+    </React.Fragment>
   );
 };
 export default TodoApp;
