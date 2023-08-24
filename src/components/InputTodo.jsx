6472759a50d3a829a40a7e1371f0bf1b15ef4a57
AliReza <ali.reza.buet@gmail.com>
2023-08-24 23:02:16 +0600

    Complete the addNewItem, deleteItem and unique id feature

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,4 +1,33 @@
-const InputTodo = () => {
-  return <div>input field here...</div>;
+import { useState } from "react";
+
+
+const InputTodo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+    } else {
+      alert('Please add item');
+    }
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        value={title}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  )
 };
 export default InputTodo;
